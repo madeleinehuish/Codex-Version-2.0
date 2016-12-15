d12544bc7f03421515eff8672c042354167c22d2
madeleinehuish <vasistha@hotmail.com>
2016-12-14 21:49:08 -0800

    fixed a major bug related to react router version

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { link } from 'react-router';
+import {Link} from 'react-router';
 
-class HomePage extends React.component {
-	render() {
-		return (
-			<div className="jumbotron">
-				<h1>Pluralsight Administration</h1>
-				<p>React, redux, and react router....</p>
-				<Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
-			</div>
-		);
-	}
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Administration</h1>
+        <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
